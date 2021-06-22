265b4bfffba75edc7ad5e323576d2dde5d6e553f
sara-javadzadeh <saraj@eng.ucsd.edu>
2021-06-22 12:17:24 -0700

    Update instructions on custom kraken database script

--- build_custom_kraken_database.sh
+++ build_custom_kraken_database.sh
@@ -4,8 +4,8 @@ if [ $# -ne 3 ]; then
 	echo "Usage: bash $0 <virus name> <input fasta> <base node id>"
     echo "<virus name>: virus name in lower case, e.g. hpv"
 	echo "<input fasta>: path to the input fasta file containing all references of the same virus"
-	echo "<base node id>: is the node id to label the custom reference genomes when adding to kraken2. Use 9000000 if adding a single virus type. If adding multiple virus types (e.g. HBV and HCV), indicate different base ids for each virus. for example, build HBV with base node 9000000 and HCV with base node 9100000."
-	exit 1
+	echo "<base node id>: As the viral references are added to Kraken2 externally, the taxonomy information for the custom reference file should be provided (such as node IDs). Our custom database script creates taxonomy IDs for all the references in the input FASTA file, starting from the <base node id> and incrementing by 1. For <base node id>, choose a large number that is not repeated in the default taxonomy labeling in Kraken. Use 9000000 if only working on one virus type. To avoid confusions in the analysis, we suggest using a different base node ID if you are working with multiple viruses, even when creating different custom Kraken2 databases. For example, use 9000000 for HBV and 9100000 for HCV, if you are working on these two viruses."
+    exit 1
 fi
 VIRUS_NAME=$1
 LIBRARY_INPUT=$2
